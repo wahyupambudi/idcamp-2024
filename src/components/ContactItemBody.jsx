5d623b2248f38a0ca3218e47d97eb005f7785081
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-10-28 22:16:20 +0700

    feat - finish contacts-app

--- src/components/ContactItemBody.jsx
+++ src/components/ContactItemBody.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-function ContactItemBody({ name, tag }) {
+function ContactItemBody({ id, name, tag }) {
   return (
     <div className="contact-item__body">
+      <h3 className="contact-item__title">id: {id}</h3>
       <h3 className="contact-item__title">{name}</h3>
       <p className="contact-item__username">@{tag}</p>
     </div>
