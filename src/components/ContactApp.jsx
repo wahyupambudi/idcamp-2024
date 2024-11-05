9aaa0f9dc32af3bb857e614bf6f6211f5f52822f
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-11-05 22:01:40 +0700

    feat - add react fragment and add contact

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
+import ContactInput from './ContactInput';
 
 // function ContactApp() {
 //   const contacts = getData();
@@ -21,6 +22,7 @@ class ContactApp extends React.Component {
       contacts: getData()
     };
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
   }
 
   onDeleteHandler(id) {
@@ -28,12 +30,33 @@ class ContactApp extends React.Component {
     this.setState({ contacts });
   }
 
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl: '/images/default.jpg'
+          }
+        ]
+      };
+    });
+  }
+
   render() {
     return (
-      <div className="contact-app">
-        <h1>Daftar Kontak</h1>
-        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
-      </div>
+      <>
+        <div className="contact-app">
+          <h1>Aplikasi Kontak</h1>
+          <h2>Tambah Kontak</h2>
+          <ContactInput addContact={this.onAddContactHandler} />
+          <h2>Daftar Kontak</h2>
+          <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
+        </div>
+      </>
     );
   }
 }
